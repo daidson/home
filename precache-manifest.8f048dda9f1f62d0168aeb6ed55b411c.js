79ae7ed3de3567e268381613a54a0f3c998ba040
Daidson Alves <daidson.alves@gmail.com>
2020-08-12 15:01:16 -0300

    Updates

---
+++ precache-manifest.8f048dda9f1f62d0168aeb6ed55b411c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820732f36ec96a924e43dbe26b079b88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18ee693fe33acf9aeb4f",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d4505171a02eb15bd690",
+    "url": "/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "18ee693fe33acf9aeb4f",
+    "url": "/static/js/2.11047eb6.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.11047eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4505171a02eb15bd690",
+    "url": "/static/js/main.f64fe91a.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "9f1b95bb27c16541abee426933982e59",
+    "url": "/static/media/resume.9f1b95bb.pdf"
+  }
+]);
